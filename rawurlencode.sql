4b9d7f8dae5c7c9a94ac6bec428ebefc13a3b93a
xerno <info@xerno.cz>
2015-02-11 03:20:17 +0100

    first commit

---
+++ rawurlencode.sql
@@ -0,0 +1,64 @@
+DROP FUNCTION IF EXISTS rawurlencode;
+
+DELIMITER $$
+
+CREATE FUNCTION rawurlencode(input TEXT)
+   RETURNS TEXT
+   COMMENT "URL-encode according to RFC 3986, like PHP's rawurlencode()."
+   LANGUAGE SQL
+   DETERMINISTIC
+   BEGIN
+      DECLARE output TEXT;
+      DECLARE length INT;
+      DECLARE i INT;
+      DECLARE chr VARCHAR(1);  -- char is not available, it is a keyword
+      DECLARE temp VARCHAR(10);
+      DECLARE temp_length TINYINT;
+      DECLARE p TINYINT;
+
+      IF input IS NULL THEN  -- protection against freezing
+         RETURN input;
+      ELSEIF input REGEXP "[^A-Za-z0-9_.~-]+$" THEN  -- speed-up
+         RETURN input;
+      END IF;
+
+      SET output = "";
+      SET length = CHAR_LENGTH(input);
+      SET i = 1;
+
+      chars: LOOP
+         IF i>length THEN
+            LEAVE chars;
+         END IF;
+
+         SET chr = SUBSTRING(input, i, 1);
+         IF chr REGEXP "[A-Za-z0-9_.~-]" THEN
+            SET output = CONCAT(output, chr);
+         ELSE
+
+            SET temp = HEX(chr);
+            SET temp_length = LENGTH(temp);
+            IF temp_length=2 THEN  -- speed-up
+               SET output = CONCAT(output, "%", temp);
+            ELSE
+
+               SET p = 1;
+               percentage: LOOP
+                  IF p>temp_length THEN
+                     LEAVE percentage;
+                  END IF;
+                  SET output = CONCAT(output, "%", SUBSTRING(temp, p, 2));
+                  SET p = p+2;
+               END LOOP percentage;
+
+            END IF;
+
+         END IF;
+
+         SET i = i+1;
+
+      END LOOP chars;
+
+      RETURN output;
+
+   END;
